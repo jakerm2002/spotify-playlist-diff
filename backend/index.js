92cccfcb970b12bbbd12a238d5fab3bdaacc5d3e
Jake Medina <jake.medina02@gmail.com>
2023-01-02 01:30:05 -0600

    removed sensitive data from sample output

--- backend/index.js
+++ backend/index.js
@@ -93,10 +93,6 @@ app.listen(
     PORT,
     () => {
         console.log('hello')
-        var client_id = process.env.CLIENT_ID;
-        var client_secret = process.env.CLIENT_SECRET;
-        console.log(client_id);
-        console.log(client_secret);
         authenticate();
     }
 )
