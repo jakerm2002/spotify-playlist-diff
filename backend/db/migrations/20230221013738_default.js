121b5592ade1134a44aa6bd9d5dbe92a75ff944e
Jake Medina <jake.medina02@gmail.com>
2023-02-20 19:40:06 -0600

    new knex migration file

---
+++ backend/db/migrations/20230221013738_default.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('sessions', require('./schemas/Session'))
+        .createTable('playlists', require('./schemas/Playlist'))
+        .createTable('tracks', require('./schemas/Track'))
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('sessions')
+        .dropTableIfExists('playlists')
+        .dropTableIfExists('tracks');
+};
