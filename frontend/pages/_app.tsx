6e5a0f784cbebb494c07eb5cc53363f280518757
Jake Medina <jake.medina02@gmail.com>
2023-02-28 20:40:37 -0600

    use spotify fonts

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,16 @@ import type { AppProps } from 'next/app'
 import { ThemeOptions } from '@mui/material/styles';
 import { ThemeProvider, useTheme, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+// const localFont = require('@next/font/local');
+import localFont from '@next/font/local';
+const myFont = localFont({ 
+  src: '../public/fonts/circular-medium.ttf',
+  weight: '400',
+  style: 'normal',
+  display: 'swap'
+});
+
+// import CircularMedium from "../public/fonts/circular-medium.woff";
 
 declare module '@mui/material/styles' {
   interface Theme {
@@ -33,6 +43,23 @@ const theme = createTheme({
       paper: '#171717',
     },
   },
+  typography: {
+    fontFamily: 'circular-medium',
+  },
+  // components: {
+  //   MuiCssBaseline: {
+  //     styleOverrides: `
+  //       @font-face {
+  //         font-family: 'circular-medium';
+  //         font-style: normal;
+  //         font-display: swap;
+  //         font-weight: 400;
+  //         src: local('Circular'), local('circular-medium'), url(${myFont}) format('ttf');
+  //         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+  //       }
+  //     `,
+  //   },
+  // },
 });
 
 export default function App({ Component, pageProps }: AppProps) {
