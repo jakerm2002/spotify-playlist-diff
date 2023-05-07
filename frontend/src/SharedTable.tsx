7d5b3555c3323cb77483a3b1c0b5ee257b62bfb3
Jake Medina <jake.medina02@gmail.com>
2023-05-06 19:44:39 -0500

    gave some margins to the shared table

--- frontend/src/SharedTable.tsx
+++ frontend/src/SharedTable.tsx
@@ -11,12 +11,13 @@ const SharedTable: React.FC<SharedTableProps> = ({ rows }) => {
 
     const columns: GridColDef[] = [
         { field: 'playlist_order', headerName: '1st appearance in P1', width: 200 },
-        {   field: 'cover_art_url', 
+        {
+            field: 'cover_art_url',
             headerName: 'Art',
             width: 130,
             sortable: false,
             filterable: false,
-            renderCell: (params) => <img src={params.value} width='45' height='45'/>
+            renderCell: (params) => <img src={params.value} width='45' height='45' />
         },
         { field: 'track_name', headerName: 'Track', width: 250 },
         { field: 'album_name', headerName: 'Album', width: 250 },
@@ -33,22 +34,24 @@ const SharedTable: React.FC<SharedTableProps> = ({ rows }) => {
 
     return (
         <React.Fragment>
-            <Typography variant="h5">
-                Shared Tracks
-            </Typography>
-            <div style={{ height: 400, width: '100%' }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    rowsPerPageOptions={[20, 30, 50, 100]}
-                    getRowId={(row) => row.spotify_track_id}
-                    initialState={{
-                        sorting: {
-                          sortModel: [{ field: 'playlist_order', sort: 'asc' }],
-                        },
-                    }}
-                    disableSelectionOnClick
-                />
+            <div style={{marginLeft: '2.5%', marginRight: '2.5%'}}>
+                <Typography variant="h5">
+                    Shared Tracks
+                </Typography>
+                <div style={{ height: 400, width: '100%' }}>
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        rowsPerPageOptions={[20, 30, 50, 100]}
+                        getRowId={(row) => row.spotify_track_id}
+                        initialState={{
+                            sorting: {
+                                sortModel: [{ field: 'playlist_order', sort: 'asc' }],
+                            },
+                        }}
+                        disableSelectionOnClick
+                    />
+                </div>
             </div>
         </React.Fragment>);
 }
