353f9a8b7e0da5e5a8e59b2b46c765d3c26cd843
Jake Medina <jake.medina02@gmail.com>
2024-12-13 12:57:38 -0600

    cloud sql instance

---
+++ terraform/cloud_sql.tf
@@ -0,0 +1,25 @@
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+# locals {
+#   onprem = ["192.168.1.2", "192.168.2.3"]
+# }
+
+resource "google_sql_database_instance" "mysql" {
+  name             = "spotify-playlist-diff-db-${random_id.db_name_suffix.hex}"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  settings {
+    tier = "	db-custom-1-3840"
+    disk_size = 10
+    disk_autoresize = false
+  }
+}
+
+resource "google_sql_user" "users" {
+  name     = "express"
+  instance = google_sql_database_instance.mysql.name
+  password = var.EXPRESS_SQL_USER_PASSWORD
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,7 @@
 variable "GITHUB_TOKEN_SECRET" {
   sensitive = true  
 }
+
+variable "EXPRESS_SQL_USER_PASSWORD" {
+  sensitive = true
+}
